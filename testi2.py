031599c2ecbe4d62a32693a5959f31678924a8a6
Laghitus <velimatti92@hotmail.com>
2017-11-28 13:09:26 +0200

    Update testi2.py

--- testi2.py
+++ testi2.py
@@ -2,7 +2,7 @@ import serial
 import time
 import MySQLdb
 
-#establish connection to MySQL. You'll have to change this for your database.
+#establish connection to MySQL.
 dbConn = MySQLdb.connect("localhost","root","root","saaasema") or die ("could not connect to database")
 #open a cursor to the database
 cursor = dbConn.cursor()
@@ -14,13 +14,13 @@ def read_serial(ser):
         time.sleep(0.5)
     return ser.readline()
 
-#device = "/dev/ttyACM0"
+
 buadrate = 9600
 ser = serial.Serial("/dev/ttyACM0", buadrate, timeout=1)
 time.sleep(2) #wait for the Arduino to init
 
 while True:
-    ser.write("Hello\n") #On the Arduino I'm reading with: Serial.readStringUntil('\n');
+    ser.write("Hello\n")
 
     data = read_serial(ser)
     print "read: %s" % data
@@ -28,7 +28,7 @@ while True:
 
 
     pieces = data.split("\t")
-    #print "read: %s" % pieces[1]
+    
 
     try:
         cursor.execute("INSERT INTO data (kosteus,lampotila) VALUES (%s,%s)", (pieces[0],pieces[1].rstrip()))
